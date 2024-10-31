ba1a1d2d0a789be06e5a89a3df9878829b01423e
Vishnu <vishnurp3@gmail.com>
2024-10-31 19:13:34 +0530

    Initial commit: Project setup with Spring Initializr

---
+++ src/main/java/com/vishnurp3/bookmanagementservice/BookManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vishnurp3.bookmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vishnurp3/bookmanagementservice/BookManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnurp3.bookmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
